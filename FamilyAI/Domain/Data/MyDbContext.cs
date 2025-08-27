f6f285bfacad1248bdb3d700698e19c1947e4ad5
DFWDino <Shane.Newsom@gmail.com>
2025-08-26 19:54:31 -0500

    Started to set up schema.

---
+++ FamilyAI/Domain/Data/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FamilyAI.Domain.Data
+{
+    public class MyDbContext : DbContext
+    {
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- FamilyAI/Program.cs
+++ FamilyAI/Program.cs
@@ -1,10 +1,18 @@
+using FamilyAI.Domain.Data;
 using FamilyAI.Presentation;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// FIX: Ensure you have installed the NuGet package ''
+// If not, run: dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+
+builder.Services.AddDbContext<MyDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -13,7 +21,6 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
-
 
 app.UseAntiforgery();
 
